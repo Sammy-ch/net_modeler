4a3d5427cbd0bf219e6641784fea320fec27282d
Sammy-ch <sammystcherubin@gmail.com>
2025-07-10 00:34:16 +0200

    added network link and node structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+#[derive(Debug)]
+struct Node {
+    id: String,
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct Link {
+    link_id: String,
+    source_node: Node,
+    destination_node: Node,
+    capacity: u8,
+    weight: u8,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    Ok(())
 }
